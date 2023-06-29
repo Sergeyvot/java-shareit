aa13a67e93b2e36178efc95d43e3887db1507f60
unknown <SVyakimchuk@yandex.ru>
2023-06-29 23:14:55 +0400

    fix: исправить замечания по checkstyle

--- src/main/java/ru/practicum/shareit/item/model/Comment.java
+++ src/main/java/ru/practicum/shareit/item/model/Comment.java
@@ -30,8 +30,8 @@ public class Comment {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof User)) return false;
-        return id != null && id.equals(((User) o).getId());
+        if (!(o instanceof Comment)) return false;
+        return id != null && id.equals(((Comment) o).getId());
     }
 
     @Override
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -35,8 +35,8 @@ public class Item {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof User)) return false;
-        return id != null && id.equals(((User) o).getId());
+        if (!(o instanceof Item)) return false;
+        return id != null && id.equals(((Item) o).getId());
     }
 
     @Override
